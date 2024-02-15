0a5895ce68ca7196157647b1a5e772607afab913
Andrey Morogin <andreandreandre10071998@yandex.ru>
2024-02-16 00:52:57 +0300

    ASTONJS-2 [feat]: add function compare

--- homework/2.js
+++ homework/2.js
@@ -5,3 +5,25 @@ function getLength(type) {
       console.log(0)
   }
 }
+
+// Первое решение со сложностью О(n), где n - кол-во узлов наименьшего дерева.
+/*function compare(node1, node2) {
+  const arr1 = [node1];
+  const arr2 = [node2];
+
+  for (let i = 0; i < arr1.length || i < arr2.length; i++) {
+      if (!arr1[i] || !arr2[i] || arr1[i].value !== arr2[i].value) {
+          return false
+      }
+      if (arr1[i].left) arr1.push(arr1[i].left);
+      if (arr1[i].right) arr1.push(arr1[i].right);
+      if (arr2[i].left) arr2.push(arr2[i].left);
+      if (arr2[i].right) arr2.push(arr2[i].right);
+  }
+  return true
+}*/
+
+// Второе решение со сложностью О(1).При создании строк неопределённые узлы теряются, но проверке это не мешает.
+function compare(node1, node2) {
+  return JSON.stringify(node1) === JSON.stringify(node2)
+}
