58aa41bac2ce5b57ef699d3f172fa37bcad045de
Andrey Morogin <andreandreandre10071998@yandex.ru>
2024-02-28 20:41:29 +0300

    ASTONJS-5 [chore]: fix the algorithm

--- homework/5.js
+++ homework/5.js
@@ -46,10 +46,7 @@ class HyperPerson extends SuperPerson {
 //1.
 function firstSum(arr, total) {
   for (let i = 0; i < arr.length; i++) {
-      for (let j = 0; j < arr.length; j++) {
-          if (j === i) {
-              continue;
-          }
+      for (let j = i + 1; j < arr.length; j++) {
           if (arr[i] + arr[j] === total) {
               return [ arr[i], arr[j] ]
           }
