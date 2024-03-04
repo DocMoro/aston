34fca76d13ecfae5b02f6e1fccbf37f1c2f7cd85
Andrey Morogin <andreymorogin@mail.ru>
2024-03-05 01:49:45 +0300

    ASTONJS-7 [feat]: speed up the pattern func x10

--- homework/7.js
+++ homework/7.js
@@ -4,13 +4,12 @@ function pattern(n) {
     if (n < 1) {
         return
     }
-    const arr = [1];
     const cash = ['1'];
     for (let i = 0; i < n - 1; i++) {
-        console.log(cash[i]);
-        const num = i + 2;
-        const str = arr.join('') + num + [...arr].reverse().join('');
-        arr.push(num);
+        console.log(cash[i])
+        const length = cash[i].length / 2; 
+        const str = cash[i].slice(0, Math.ceil(length)) 
+        + (i + 2) + cash[i].slice(Math.floor(length));
         cash.push(str);
     }
     cash.reverse().forEach(s => console.log(s))
